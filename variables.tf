944099f6292a2947bc6d86ee5ed050b354143e6a
Miguel Garcia <miguel.garcia@bi-geek.com>
2019-11-27 08:07:22 +0100

    first version for module

---
+++ variables.tf
@@ -0,0 +1,53 @@
+variable "snsName" {
+  description = "Name of the SNS Topic"
+  default     = ""
+  type        = "string"
+}
+
+variable "sqsName" {
+  description = "Name of the SQS queue"
+  default     = ""
+  type        = "string"
+}
+
+variable "sqsFifo" {
+  description = "Makes SQS FIFO or not"
+  default     = "false"
+  type        = "string"
+}
+
+variable "snsSubscribe" {
+  description = "Enable/disable SNS subscription for SQS queues"
+  default     = true 
+  type        = bool 
+}
+
+variable "sesCreate" {
+  description = "Enable/disable SES creation"
+  default     = false
+  type        = bool
+}
+
+variable "createSns" {
+  description = "Enable/disable resource creation"
+  default     = true
+  type        = bool
+}
+
+variable "createSqs" {
+  description = "Enable/disable resource creation"
+  default     = true
+  type        = bool
+}
+
+variable "sesDomain" {
+  description = "Domain for creating SES"
+  default     = ""
+  type        = "string"
+}
+
+variable "zone_id" {
+  description = "ID from the Route53 zone where we're going to create the DNS records for SES"
+  default     = ""
+  type        = "string"
+}
