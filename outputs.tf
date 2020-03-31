7f62c89b6b38e3d9a8bb740b73be99978d934289
Miguel Garcia <miguel.garcia@bi-geek.com>
2020-03-31 15:30:55 +0200

    adding new output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
-output "topic_arn" {
-  #value = aws_sns_topic.sns_topic[0].id
+output "this_sns_arn" {
+  description = "The ARN of the SNS topic. Will be of format arn:aws:sns:::topicname."
   value = concat(aws_sns_topic.sns_topic[*].id, [""])[0]
+}
+
+output "this_sqs_arn" {
+  description = "The ARN of the SQS queue. Will be of format arn:aws:sqs:::queuename."
+  value = 
 }
